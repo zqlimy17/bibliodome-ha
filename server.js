3a18802d1b130443238aae9f2ff4f7f42c07cce0
zqlimy17 <zqlimy@gmail.com>
2019-11-20 21:25:04 +0800

    edited .env

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 
 // Global configuration
-const mongoURI = "mongodb://localhost:27017/bibliodome";
+const mongoURI = process.env.MONGODB_URI;
 const db = mongoose.connection;
 
 // Mongoose Deprecation Warnings
